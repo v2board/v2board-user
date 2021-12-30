8561591d64bab4fb8cf6c101ef188e7b5e490d6c
object <object@objects-iMac.local>
2021-12-31 01:49:12 +0800

    update

---
+++ env.example.js
@@ -0,0 +1,18 @@
+window.settings = {
+  // 站点标题
+  title: 'V2Board',
+  // 站点描述
+  description: 'V2Board is best',
+  // API
+  host: '',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: '',
+  // crisp
+  crisp_id: ''
+}
